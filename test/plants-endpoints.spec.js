30945fe2da8f52c0144b3f8ce888c356c71afe82
Angela Masajo <aabmasajo@gmail.com>
2021-03-04 19:17:51 -0600

    tests passing

--- test/plants-endpoints.spec.js
+++ test/plants-endpoints.spec.js
@@ -68,5 +68,99 @@ describe('Plants Endpoints', function() {
           .expect(404, { error: { message: `Plant doesn't exist` } })
       })
     })
+
+    context('Given there are plants in the database', () => {
+      const testUsers = makeUsersArray()
+      const testPlants = makePlantsArray()
+
+      beforeEach('insert plants', () => {
+        return db
+          .into('users')
+          .insert(testUsers)
+          .then(() => {
+            return db
+              .into('plants')
+              .insert(testPlants)
+          }) 
+      })
+
+      it('responds with 200 and the specified plant', () => {
+        const plantId = 2
+        const expectedPlant = testPlants [plantId - 1]
+        return supertest(app)
+          .get(`/api/plants/${plantId}`)
+          .expect(200, expectedPlant)
+      })
+    })
   })
+
+  // describe(`POST /api/plants`, () => {
+  //   const testUsers = makeUsersArray();
+  //   beforeEach('insert malicious article', () => {
+  //     return db
+  //       .into('blogful_users')
+  //       .insert(testUsers)
+  //   })
+    
+  //   it(`creates an article, responding with 201 and the new article`, function() {
+  //     this.retries(3)
+  //     const newArticle = {
+  //       title: 'Test new article',
+  //       style: 'Listicle',
+  //       content: 'Test new article content...'
+  //     }
+  //     return supertest(app)
+  //       .post('/api/articles')
+  //       .send(newArticle)
+  //       .expect(201)
+  //       .expect(res => {
+  //         expect(res.body.title).to.eql(newArticle.title)
+  //         expect(res.body.style).to.eql(newArticle.style)
+  //         expect(res.body.content).to.eql(newArticle.content)
+  //         expect(res.body).to.have.property('id')
+  //         expect(res.headers.location).to.eql(`/api/articles/${res.body.id}`)
+  //         const expected = new Date().toLocaleString()
+  //         const actual = new Date(res.body.date_published).toLocaleString()
+  //         expect(actual).to.eql(expected)
+  //       })
+  //       .then(res =>
+  //         supertest(app)
+  //           .get(`/api/articles/${res.body.id}`)
+  //           .expect(res.body)
+  //       )
+  //   })
+
+  //   const requiredFields = ['title', 'style', 'content']
+
+  //   requiredFields.forEach(field => {
+  //     const newArticle = {
+  //       title: 'Test new article',
+  //       style: 'Listicle',
+  //       content: 'Test new article content...'
+  //     }
+
+  //     it(`responds with 400 and an error message when the '${field}' is missing`, () => {
+  //       delete newArticle[field]
+
+  //       return supertest(app)
+  //         .post('/api/articles')
+  //         .send(newArticle)
+  //         .expect(400, {
+  //           error: { message: `Missing '${field}' in request body` }
+  //         })
+  //     })
+  //   })
+
+  //   it('removes XSS attack content from response', () => {
+  //     const { maliciousArticle, expectedArticle } = makeMaliciousArticle()
+  //     return supertest(app)
+  //       .post(`/api/articles`)
+  //       .send(maliciousArticle)
+  //       .expect(201)
+  //       .expect(res => {
+  //         expect(res.body.title).to.eql(expectedArticle.title)
+  //         expect(res.body.content).to.eql(expectedArticle.content)
+  //       })
+  //   })
+  // })
 })
