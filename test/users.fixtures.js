48778c41b3ff4153017d5f9b2d8a2ea5834b05a6
Angela Masajo <aabmasajo@gmail.com>
2021-03-07 23:06:38 -0600

    minor change

--- test/users.fixtures.js
+++ test/users.fixtures.js
@@ -18,26 +18,24 @@ function makeUsersArray() {
   ];
 }
 
-// function makeMaliciousArticle() {
-//   const maliciousArticle = {
-//     id: 911,
-//     style: "How-to",
-//     date_published: new Date().toISOString(),
-//     title: 'Naughty naughty very naughty <script>alert("xss");</script>',
-//     content: `Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.`,
-//   };
-//   const expectedArticle = {
-//     ...maliciousArticle,
-//     title:
-//       'Naughty naughty very naughty &lt;script&gt;alert("xss");&lt;/script&gt;',
-//     content: `Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.`,
-//   };
-//   return {
-//     maliciousArticle,
-//     expectedArticle,
-//   };
-// }
+function makeMaliciousUser() {
+  const maliciousUser = {
+    id: 911,
+    user_name: "bad name here",
+    user_password: 'bad pass here <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">',
+  };
+  const expectedUser = {
+    ...maliciousUser,
+    user_name: "bad name here",
+    user_password: 'bad pass here <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">',
+  };
+  return {
+    maliciousUser,
+    expectedArticle,
+  };
+}
 
 module.exports = {
   makeUsersArray,
+  makeMaliciousUser
 };
