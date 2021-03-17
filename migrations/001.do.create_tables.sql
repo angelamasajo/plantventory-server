4379dc3f1e57864f71bc98f69fc882fff59c4223
Angela Masajo <aabmasajo@gmail.com>
2021-03-17 00:20:50 -0500

    update migrations

--- migrations/001.do.create_tables.sql
+++ migrations/001.do.create_tables.sql
@@ -16,3 +16,24 @@ CREATE TABLE user_plants (
   plant_id INTEGER REFERENCES plants(id),
   user_id INTEGER REFERENCES users(id)
 );
+
+
+--seed
+INSERT INTO plants (name, plant_type, toxicity, care_details)
+VALUES
+  ('Monstera Deliciosa', 'Tropical', 'Toxic', 'Here is the care detail for the monstera'),
+  ('Monstera Adansonii', 'Tropical', 'Toxic', 'Here is the care detail for the monstera adansonii'),
+  ('Christmas Cactus', 'Holiday', 'Pet-safe', 'Here is the care detail for the christmas cactus'),
+  ('Golden Pothos', 'Trailing', 'Toxic', 'Here is the care detail for the golden pothos'),
+  ('Zamioculcas Zamiifolia', 'Other', 'Toxic', 'Here is the care detail for the ZZ plant');
+
+
+INSERT INTO user_plants (plant_id, user_id)
+VALUES
+  (1, 1),
+  (2, 1),
+  (5, 1);
+
+INSERT INTO users (user_name, user_password)
+VALUES
+  ('DemoUser', 'Password1234');
