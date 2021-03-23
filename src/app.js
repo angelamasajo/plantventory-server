bbc19d3b3ca922e16a2f51c45bd4d4724f55c939
Angela Masajo <aabmasajo@gmail.com>
2021-03-22 21:56:25 -0500

    cors update

--- src/app.js
+++ src/app.js
@@ -13,7 +13,20 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors());
+
+const allowedOrigins = ['http://localhost:3000', 'https://plantventory-app.vercel.app/'];
+
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 app.get("/api", (req, res) => {
   res.json({ ok: true });
